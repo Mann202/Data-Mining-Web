01b055b878fc6e2986ad34852b77cc6624e988c3
Trương GIa Mẫn <21521115@gm.uit.edu.vn>
2024-06-11 21:58:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 import pickle
 
 current_page = st.session_state.get('page', 'main')
+prediction = None
 
 # Load models =========================
 #with open('./models/knn_op.pkl', 'rb') as knn_model_file:
@@ -33,86 +34,84 @@ if current_page == 'main':
     st.write("⚠️ **Please complete all of the information in the survey!**")
     st.write("This project is inspired by the COVID-19 pandemic, a historical event that changed the world three years ago. Although the pandemic has ended, its impacts continue to affect people's mentality and lives. This project aims to commemorate COVID-19 as the largest medical event of the century and convey a message about the importance of unity and human resilience in the face of immense challenges.")
 
-    with st.form(key='my_form'):
-        age = st.text_input("How old are you?")
-        if age:
-            try:
-                age = int(age)
-                if age > 150 or age < 1:
-                    st.warning("Tuổi phải nằm trong khoảng từ 1 đến 150.")
-            except ValueError:
-                st.error("Vui lòng nhập một số nguyên hợp lệ.")
+    age = st.text_input("How old are you?")
+    if age:
+        try:
+            age = int(age)
+            if age > 150 or age < 1:
+                 st.warning("Tuổi phải nằm trong khoảng từ 1 đến 150.")
+        except ValueError:
+            st.error("Vui lòng nhập một số nguyên hợp lệ.")
 
-        yes_no_diabetes = st.radio("Do you have diabetes?", ("Yes", "No"))
-        yes_no_renalChronal = st.radio("Do you have renalChronal?", ("Yes", "No"))
-        yes_no_hypertension = st.radio("Do you have hypertension?", ("Yes", "No"))
-        yes_no_pneumonia = st.radio("Do you have pneumonia?", ("Yes", "No"))
+    yes_no_diabetes = st.radio("Do you have diabetes?", ("Yes", "No"))
+    yes_no_renalChronal = st.radio("Do you have renalChronal?", ("Yes", "No"))
+    yes_no_hypertension = st.radio("Do you have hypertension?", ("Yes", "No"))
+    yes_no_pneumonia = st.radio("Do you have pneumonia?", ("Yes", "No"))
 
-        selectionBox1 = st.selectbox("What type of institution of the National Health System that provided the care?", (1,2,3,4,5,6,7,8,9,10,11,12,13))
-        selectionBox2 = st.selectbox("Do you indicate treatment in medical units of the first or second level?", (1, 2))
-        yes_no_hospitalized = st.radio("Have you ever been hospitalized?", ("Yes", "No"))
-        selectionBox3 = st.selectbox("Did you have covid test before? If you did, select the diagnosis of the test", (1,2,3,4,5,6,7))
+    selectionBox1 = st.selectbox("What type of institution of the National Health System that provided the care?", (1,2,3,4,5,6,7,8,9,10,11,12,13))
+    selectionBox2 = st.selectbox("Do you indicate treatment in medical units of the first or second level?", (1, 2))
+    yes_no_hospitalized = st.radio("Have you ever been hospitalized?", ("Yes", "No"))
+    selectionBox3 = st.selectbox("Did you have covid test before? If you did, select the diagnosis of the test", (1,2,3,4,5,6,7))
 
-        st.markdown("<hr>", unsafe_allow_html=True)
-        
-        submit_button = st.form_submit_button(label='Submit')
+    st.markdown("<hr>", unsafe_allow_html=True)
+
+    yes_no_pneumonia = 1 if yes_no_pneumonia == 'Yes' else 2
+    yes_no_diabetes = 1 if yes_no_diabetes == 'Yes' else 2
+    yes_no_hypertension = 1 if yes_no_hypertension == 'Yes' else 2
+    yes_no_renalChronal = 1 if yes_no_renalChronal == 'Yes' else 2
+    yes_no_hospitalized = 1 if yes_no_hospitalized == 'Yes' else 2
+
+    medi_unit2 = 0
+    medi_unit3 = 0
+    medi_unit4 = 0
+    medi_unit5 = 0
+    medi_unit6 = 0
+    medi_unit7 = 0
+    medi_unit8 = 0
+    medi_unit9 = 0
+    medi_unit10 = 0
+    medi_unit11 = 0
+    medi_unit12 = 0
+    medi_unit13 = 0
 
 
-        if submit_button:
+    if 1 <= selectionBox1 <= 13:
+        for i in range(2, 14):
+            if i == selectionBox1:
+                print(i)
+                globals()[f"medi_unit{i}"] = 1
+                break
+            else:
+                continue
 
-            yes_no_pneumonia = 1 if yes_no_pneumonia == 'Yes' else 2
-            yes_no_diabetes = 1 if yes_no_diabetes == 'Yes' else 2
-            yes_no_hypertension = 1 if yes_no_hypertension == 'Yes' else 2
-            yes_no_renalChronal = 1 if yes_no_renalChronal == 'Yes' else 2
-            yes_no_hospitalized = 1 if yes_no_hospitalized == 'Yes' else 2
+    classi_final2 = 0
+    classi_final3 = 0
+    classi_final4 = 0
+    classi_final5 = 0
+    classi_final6 = 0
+    classi_final7 = 0
 
-            medi_unit2 = 0
-            medi_unit3 = 0
-            medi_unit4 = 0
-            medi_unit5 = 0
-            medi_unit6 = 0
-            medi_unit7 = 0
-            medi_unit8 = 0
-            medi_unit9 = 0
-            medi_unit10 = 0
-            medi_unit11 = 0
-            medi_unit12 = 0
-            medi_unit13 = 0
+    if 1 <= selectionBox3 <= 7:
+        for i in range(2, 8):
+            if i == selectionBox3:
+                globals()[f"classi_final{i}"] = 1
+                break
+            else:
+                continue
 
-            if 1 <= selectionBox1 <= 13:
-                for i in range(2, 14):
-                    if i == selectionBox1:
-                        globals()[f"medi_unit{i}"] = 1
-                        break
-                    else:
-                        continue
-
-            classi_final2 = 0
-            classi_final3 = 0
-            classi_final4 = 0
-            classi_final5 = 0
-            classi_final6 = 0
-            classi_final7 = 0
-
-            if 1 <= selectionBox3 <= 7:
-                for i in range(2, 8):
-                    if i == selectionBox3:
-                        globals()[f"classi_final{i}"] = 1
-                        break
-                    else:
-                        continue
 
             
-            features_other = np.array([ 
-            [selectionBox2, yes_no_hospitalized, yes_no_pneumonia, age, yes_no_diabetes, yes_no_hypertension, yes_no_renalChronal, medi_unit2, medi_unit3, medi_unit4, medi_unit5, medi_unit6, medi_unit7, medi_unit8, medi_unit9, medi_unit10, medi_unit11, medi_unit12, medi_unit13, classi_final2, classi_final3, classi_final4, classi_final5, classi_final6, classi_final7]], dtype= object)
-            
-            model = st.selectbox( 'Pick a model?', ('Gaussian Naives Bayes', 'Bernoulli Naives Bayes','Decicion Tree', 'Random Forest', 'K-Nearest Neighboor'), key="select_model") 
-            btn = st.form_submit_button("Predict")
-            if btn:
-                if model == 'Gaussian Naives Bayes':
-                    prediction = predict_high_risk_death(gnb_model, features_other)
-                elif model == 'Bernoulli Naives Bayes':
-                    prediction = predict_high_risk_death(bnb_model, features_other)
+    features_other = np.array([ 
+    [selectionBox2, yes_no_hospitalized, yes_no_pneumonia, age, yes_no_diabetes, yes_no_hypertension, yes_no_renalChronal, medi_unit2, medi_unit3, medi_unit4, medi_unit5, medi_unit6, medi_unit7, medi_unit8, medi_unit9, medi_unit10, medi_unit11, medi_unit12, medi_unit13, classi_final2, classi_final3, classi_final4, classi_final5, classi_final6, classi_final7]], dtype= object)
+
+    model = st.selectbox( 'Pick a model?', ('Gaussian Naives Bayes', 'Bernoulli Naives Bayes','Decicion Tree', 'Random Forest', 'K-Nearest Neighboor'), key="select_model") 
+    btn = st.button("Predict")
+    if btn:
+        print(features_other)
+        if model == 'Gaussian Naives Bayes':
+            prediction = predict_high_risk_death(gnb_model, features_other)
+        elif model == 'Bernoulli Naives Bayes':
+            prediction = predict_high_risk_death(bnb_model, features_other)
                 #elif model == 'Decicion Tree':
                     #prediction = predict_high_risk_death(dt_model, features_other)
                 #elif model == 'Random Forest':
@@ -120,65 +119,30 @@ if current_page == 'main':
                 #elif model == 'K-Nearest Neighboor':
                     #prediction = predict_high_risk_death(knn_model, features_other)
 
-                if prediction is not None: 
-                    if prediction[0] == 1:  
-                        st.session_state['page'] = 'high-risk'
-                    else:
-                        st.session_state['page'] = 'low-risk'
-else:
-    # Title của ứng dụng Streamlit
+    if prediction is not None: 
+        print(prediction)
+        if prediction[0] == 1:  
+            st.session_state['page'] = 'high-risk'
+        else:
+            st.session_state['page'] = 'low-risk'
+elif current_page == 'high-risk':
+    st.title("Machine Learning-Based Classification for Predicting COVID-19 Mortality Rates")
+
+
+    st.markdown('<p style="color:#FF007F; font-size: 27px;"><strong>HIGH RISK OF DEATH!</strong></p>', unsafe_allow_html=True)
+
+    st.markdown('<p style="color:#FFFF99; font-size: 22px;"><em>The mortality rate for this patient is high when they contract COVID-19</em></p>', unsafe_allow_html=True)
+    if st.button('BACK'):
+        go_back()
+elif current_page == 'low-risk': 
     st.title("Machine Learning-Based Classification for Predicting COVID-19 Mortality Rates")
 
     # Thêm CSS
-    st.markdown("""
-        <style>
-        .reportview-container {
-            background: #F7F7F9;
-            padding-top: 2rem;
-        }
-        .custom-box {
-            background-color: #EFEFEF;
-            padding: 50px;
-            border-radius: 10px;
-            box-shadow: 2px 2px 5px rgba(0,0,0,0.1);
-        }
-        .alert {
-            color: black;  # Đổi màu tiêu đề thành đen
-            font-size: 30px;
-            font-weight: bold;
-            text-align: center;
-            margin-bottom: 20px;
-        }
-        .subtext {
-            color: #333;
-            font-size: 18px;
-            text-align: center;
-            margin-bottom: 30px;
-        }
-        .button {
-            display: flex;
-            justify-content: center;
-        }
-        .btn {
-            background-color: #1A535C;
-            color: white;
-            font-size: 16px;
-            font-weight: bold;
-            padding: 10px 20px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-        .btn:hover {
-            background-color: #0A3D44;
-        }
-        </style>
-    """, unsafe_allow_html=True)
+    st.markdown('<p style="color:#74C365; font-size: 27px;"><strong>HIGH RISK OF DEATH!</strong></p>', unsafe_allow_html=True)
 
-    # Tạo giao diện
-    st.markdown("<div class='custom-box'>", unsafe_allow_html=True)
-    st.markdown("<div class='alert'>HIGH RISK OF DEATH!</div>", unsafe_allow_html=True)
-    st.markdown("<div class='subtext'>The mortality rate for this patient is high when they contract COVID-19</div>", unsafe_allow_html=True)
+    st.markdown('<p style="color:#AEC6CF; font-size: 22px;"><em>The mortality rate for this patient is high when they contract COVID-19</em></p>', unsafe_allow_html=True)
     if st.button('BACK'):
         go_back()
-    st.markdown("</div>", unsafe_allow_html=True)
+else:
+    if st.button('BACK'):
+        go_back()
