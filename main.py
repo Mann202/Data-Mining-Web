23433a867fabe59d13bf037f3371a87ea2c65ba7
Trương GIa Mẫn <21521115@gm.uit.edu.vn>
2024-06-12 21:35:59 +0700

    final

--- main.py
+++ main.py
@@ -7,16 +7,21 @@ import pickle
 current_page = st.session_state.get('page', 'main')
 prediction = None
 
+with open('./scaler/min_max_scaler.pkl (for KNN).pkl', 'rb') as knn_model_scaler:
+    knn_model_age_scaler = pickle.load(knn_model_scaler)
+with open('./scaler/minmax_scaler.pkl (for DT, RF, NB).pkl', 'rb') as other_model_scaler:
+    other_model_age_scaler = pickle.load(other_model_scaler)
+
 # Load models =========================
-#with open('./models/knn_op.pkl', 'rb') as knn_model_file:
-    #knn_model = pickle.load(knn_model_file)
+with open('./models_opt/knn_op.pkl', 'rb') as knn_model_file:
+    knn_model = pickle.load(knn_model_file)
 with open('./models_opt/gnb_op.pkl', 'rb') as gnb_model_file:
     gnb_model = pickle.load(gnb_model_file)
 with open('./models_opt/bnb_op.pkl', 'rb') as bnb_model_file:
     bnb_model = pickle.load(bnb_model_file)
 with open('./models_opt/dt_op.pkl', 'rb') as dt_model_file:
     dt_model = pickle.load(dt_model_file)
-with open('./models/knn_op.pkl', 'rb') as rf_model_file:
+with open('./models/rf_op.pkl', 'rb') as rf_model_file:
     rf_model = pickle.load(rf_model_file)
 # =========================================
 
@@ -34,14 +39,9 @@ if current_page == 'main':
     st.write("⚠️ **Please complete all of the information in the survey!**")
     st.write("This project is inspired by the COVID-19 pandemic, a historical event that changed the world three years ago. Although the pandemic has ended, its impacts continue to affect people's mentality and lives. This project aims to commemorate COVID-19 as the largest medical event of the century and convey a message about the importance of unity and human resilience in the face of immense challenges.")
 
-    age = st.text_input("How old are you?")
-    if age:
-        try:
-            age = int(age)
-            if age > 150 or age < 1:
-                 st.warning("Tuổi phải nằm trong khoảng từ 1 đến 150.")
-        except ValueError:
-            st.error("Vui lòng nhập một số nguyên hợp lệ.")
+    age = st.number_input('## Age',min_value=1, max_value=150, step=1)
+    scaled_age_KNN = knn_model_age_scaler.transform([[age]])
+    scaled_age_other = other_model_age_scaler.transform([[age]])
 
     yes_no_diabetes = st.radio("Do you have diabetes?", ("Yes", "No"))
     yes_no_renalChronal = st.radio("Do you have renalChronal?", ("Yes", "No"))
@@ -102,8 +102,10 @@ if current_page == 'main':
 
             
     features_other = np.array([ 
-    [selectionBox2, yes_no_hospitalized, yes_no_pneumonia, age, yes_no_diabetes, yes_no_hypertension, yes_no_renalChronal, medi_unit2, medi_unit3, medi_unit4, medi_unit5, medi_unit6, medi_unit7, medi_unit8, medi_unit9, medi_unit10, medi_unit11, medi_unit12, medi_unit13, classi_final2, classi_final3, classi_final4, classi_final5, classi_final6, classi_final7]], dtype= object)
+    [selectionBox2, yes_no_hospitalized, yes_no_pneumonia, scaled_age_other, yes_no_diabetes, yes_no_hypertension, yes_no_renalChronal, medi_unit2, medi_unit3, medi_unit4, medi_unit5, medi_unit6, medi_unit7, medi_unit8, medi_unit9, medi_unit10, medi_unit11, medi_unit12, medi_unit13, classi_final2, classi_final3, classi_final4, classi_final5, classi_final6, classi_final7]], dtype= object)
 
+    features_KNN = np.array([ 
+    [selectionBox2, yes_no_hospitalized, yes_no_pneumonia, scaled_age_KNN, yes_no_diabetes, yes_no_hypertension, yes_no_renalChronal, medi_unit2, medi_unit3, medi_unit4, medi_unit5, medi_unit6, medi_unit7, medi_unit8, medi_unit9, medi_unit10, medi_unit11, medi_unit12, medi_unit13, classi_final2, classi_final3, classi_final4, classi_final5, classi_final6, classi_final7]], dtype= object)
     model = st.selectbox( 'Pick a model?', ('Gaussian Naives Bayes', 'Bernoulli Naives Bayes','Decision Tree', 'Random Forest', 'K-Nearest Neighboor'), key="select_model") 
     btn = st.button("Predict")
     if btn:
@@ -116,6 +118,8 @@ if current_page == 'main':
             prediction = predict_high_risk_death(dt_model, features_other)
         elif model == 'Random Forest':
             prediction = predict_high_risk_death(rf_model, features_other)
+        elif model == 'K-Nearest Neighboor':
+            prediction = predict_high_risk_death(knn_model, features_KNN)
 
 
     if prediction is not None: 
@@ -135,11 +139,9 @@ elif current_page == 'high-risk':
         go_back()
 elif current_page == 'low-risk': 
     st.title("Machine Learning-Based Classification for Predicting COVID-19 Mortality Rates")
+    st.markdown('<p style="color:#74C365; font-size: 27px;"><strong>LOW RISK OF DEATH!</strong></p>', unsafe_allow_html=True)
 
-    # Thêm CSS
-    st.markdown('<p style="color:#74C365; font-size: 27px;"><strong>HIGH RISK OF DEATH!</strong></p>', unsafe_allow_html=True)
-
-    st.markdown('<p style="color:#AEC6CF; font-size: 22px;"><em>The mortality rate for this patient is high when they contract COVID-19</em></p>', unsafe_allow_html=True)
+    st.markdown('<p style="color:#AEC6CF; font-size: 22px;"><em>The mortality rate for this patient is low when they contract COVID-19</em></p>', unsafe_allow_html=True)
     if st.button('BACK'):
         go_back()
 else:
